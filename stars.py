8b80e37281b169e89fd3cc7596fae955b710e171
Spencer U <spencerunitt0@gmail.com>
2020-06-23 18:29:23 -0700

    Add files via upload

--- stars.py
+++ stars.py
@@ -3,14 +3,22 @@ import turtle
 chelsea = turtle.Turtle()
 chelsea.getscreen().bgcolor("#994444")
 
+chelsea.penup()
+chelsea.goto(-200, 100)
+chelsea.pendown()
+
+chelsea.speed(0)
+
 def star(turtle, size):
 	if size <= 10:
 		return 
 	else:	
+		turtle.begin_fill()
 		for i in range(5):
 			turtle.forward(size)
 			star(turtle, size/3)
 			turtle.left(216)
+		turtle.end_fill()	
 
 
 star(chelsea, 360)
